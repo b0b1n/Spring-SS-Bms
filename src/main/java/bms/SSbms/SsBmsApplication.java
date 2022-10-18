6ee680ba287fe4369db687f907600ec7ae3de4ce
b0b1n <sohaibskious@gmail.com>
2022-10-18 18:02:00 +0200

    Project setup

--- src/main/java/bms/SSbms/SsBmsApplication.java
+++ src/main/java/bms/SSbms/SsBmsApplication.java
@@ -1,4 +1,4 @@
-package bms.SSbms;
+package BMS.SSBms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/bms/SSbms/SsBmsApplicationTests.java
+++ src/test/java/bms/SSbms/SsBmsApplicationTests.java
@@ -1,4 +1,4 @@
-package bms.SSbms;
+package BMS.SSBms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
