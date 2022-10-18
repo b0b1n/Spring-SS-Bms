f7f6f9d7e742b73cfad6b7f5fb5bbd23cd5764cd
b0b1n <sohaibskious@gmail.com>
2022-10-18 17:54:30 +0200

    Project setup

---
+++ src/test/java/bms/SSbms/SsBmsApplicationTests.java
@@ -0,0 +1,13 @@
+package bms.SSbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsBmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
